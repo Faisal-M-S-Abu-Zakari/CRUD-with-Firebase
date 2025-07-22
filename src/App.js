f96c5e423de4feac5f164edc564912281de71d98
Faisal <zkryfysl@gmail.com>
2025-07-22 15:28:44 +0300

    2. enable the router by using Routes and Route , and use BrowserRouter in index.js file to wrap the app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import Write from "./components/Write";
+
 function App() {
-  return <div className="App">New CRUD App</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Write />} />
+        <Route path="/write" element={<Write />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Write.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Write = () => {
+  return <div> Faisal write here </div>;
+};
+
+export default Write;
